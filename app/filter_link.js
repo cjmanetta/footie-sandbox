f11841c0164cfc34e14f405e70c42fd63b0ac6c3
cjmanetta <cjmanetta@gmail.com>
2016-03-18 16:39:00 -0700

    simplify footer component and add behavioral component class filterlink

--- app/filter_link.js
+++ app/filter_link.js
@@ -1,24 +1,61 @@
 import React from 'react';
-// import store from './store';
+import store from './store';
+import Link from './link';
 
-const FilterLink = ({
-	filter,
-	currentFilter,
-	children,
-	onClick
-}) => {
-	if (filter === currentFilter) {
-		return <span>{children}</span>;
+class FilterLink extends React.Component {
+	componentDidMount() {
+		this.unsubscribe = store.subscribe(() =>
+			this.forceUpdate()
+		);
 	}
-	return (
-		<a href="#"
-			onClick={event => {
-				event.preventDefault();
-				onClick(filter);
-			}}>
-			{children}
-		</a>
-	);
-};
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		const props = this.props;
+		const state = store.getState();
+
+		return (
+			<Link
+				active={ props.filter === state.visibilityFilter}
+				onClick={() =>
+					store.dispatch({
+						type: 'SET_VISIBILITY_FILTER',
+						filter: props.filter
+					})
+				}
+			>
+				{props.children}
+			</Link>
+		);
+	}
+}
+
+
+
+
+
+
+// const FilterLink = ({
+// 	filter,
+// 	currentFilter,
+// 	children,
+// 	onClick
+// }) => {
+// 	if (filter === currentFilter) {
+// 		return <span>{children}</span>;
+// 	}
+// 	return (
+// 		<a href="#"
+// 			onClick={event => {
+// 				event.preventDefault();
+// 				onClick(filter);
+// 			}}>
+// 			{children}
+// 		</a>
+// 	);
+// };
 
 export default FilterLink;
