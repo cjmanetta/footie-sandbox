9f55348181e1ebab93d2a5c9ae23f18e3093d172
cjmanetta <cjmanetta@gmail.com>
2016-03-22 09:39:37 -0700

    refactor and extract getSortedScores into actions

--- app/actions.js
+++ app/actions.js
@@ -1,20 +1,65 @@
 import React from 'react';
 
-let nextTodoId = 0;
+const getFilteredScores = (scores, player, targetArray) => {
+	targetArray = scores.filter((score) => {
+		return score.player === player.id.toString()
+	});
+	return targetArray
+};
 
-const addTodo = (text) => {
-	return {
-		type: 'ADD_TODO',
-		id: nextTodoId++,
-		text
-	};
-}
+const getFilteredScoreValues = (scores, targetArray) => {
+	scores.forEach((score, index, scores) => {
+		targetArray.push(parseInt(score.value))
+	})
+	return targetArray;
+};
 
-const setVisibilityFilter = (filter) => {
-	return {
-		type: 'SET_VISIBILITY_FILTER',
-		filter
-	};
-}
+const reduceScores = (scores) => {
+	if(scores.length > 0) {
+		return scores.reduce((prev, curr) => prev + curr);
+	} else {
+		return [0];
+	}
+};
 
-export { addTodo, setVisibilityFilter } ;
+const getSortedScores = (state) => {
+	
+	let scores = state.scores;
+	let players = state.players;
+	let sortedScores = [];
+
+	players.forEach((player, index, players) => {
+		let filteredScores = [], filteredScoreValues = [], reducedScores = [];
+
+		filteredScores = getFilteredScores(scores, player, filteredScores);
+		filteredScoreValues = getFilteredScoreValues(filteredScores, filteredScoreValues);
+		reducedScores = reduceScores(filteredScoreValues);
+
+		sortedScores.push({player: player.name, value: reducedScores});
+	});
+
+	return sortedScores.sort((a, b) => { return b.value - a.value;});
+	
+};
+
+
+
+
+// let nextTodoId = 0;
+
+// const addTodo = (text) => {
+// 	return {
+// 		type: 'ADD_TODO',
+// 		id: nextTodoId++,
+// 		text
+// 	};
+// }
+
+// const setVisibilityFilter = (filter) => {
+// 	return {
+// 		type: 'SET_VISIBILITY_FILTER',
+// 		filter
+// 	};
+// }
+
+export { getSortedScores } ;
--- app/components/Scoreboard.js
+++ app/components/Scoreboard.js
@@ -1,41 +1,6 @@
 import React from 'react';
 import ScoresList from './scoreslist';
-
-const getSortedScores = (state) => {
-	
-	let scores = state.scores;
-	let players = state.players;
-	let sortedScores = [];
-
-	players.forEach((player, index, players) => {
-		const playersScores = scores.filter((score) => {
-			return score.player === player.id.toString() 
-		});
-
-		let playersValues = [];
-
-		const pushPlayersValues = () => {
-			playersScores.forEach((score, index, playersScores) => { playersValues.push(parseInt(score.value))})
-		}
-
-		pushPlayersValues()
-
-
-		const reducedScore = () => {
-			if (playersValues.length === 0) {return playersValues = [0]}
-
-			return playersValues.reduce((prev, curr) => prev + curr);
-		}
-	
-		sortedScores.push({player: player.name, value: reducedScore()});
-	})
-
-	sortedScores.sort((a, b) => {
-      return b.value - a.value;
-    })
-
-	return sortedScores
-};
+import { getSortedScores } from '../actions';
 
 class Scoreboard extends React.Component {
 
@@ -55,7 +20,7 @@ class Scoreboard extends React.Component {
 		const state = store.getState();
 
 		return (
-			<div><ScoresList scores={getSortedScores(state)} /></div>
+			<div><ScoresList scores={ getSortedScores(state) } /></div>
 		)
 	}
 };
